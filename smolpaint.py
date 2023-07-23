35825e7360a2c02eab985d7efe7d2fe6ade3ce66
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-07-23 22:05:36 +0200

    update

--- smolpaint.py
+++ smolpaint.py
@@ -1,178 +1,86 @@
-# smolPaint GUI
-# Tiny paint program for 5x5 NeoPixel BFF Grid displays
+# smolPainter
 # by Krzysztof Krystian Jankowski
 # (c) 2023/07
 
-import tkinter as tk
-from tkinter import ttk, Scale, font
-import sys
-import os
+import machine
+import neopixel
+import utime
 
-class smolPaint:
+class smolPaint():
     def __init__(self):
-        if getattr(sys, 'frozen', False):
-            application_path = sys._MEIPASS
-        else:
-            application_path = os.path.dirname(os.path.abspath(__file__))
-        icon_path = os.path.join(application_path, 'icon.png')
+        self.pixels = neopixel.NeoPixel(machine.Pin(29),5*5)
+        self.pixels.fill((0,0,0))
+        self.pixels.write()
+        self.logo = [0,0,0,25,25,25,25,25,25,25,25,25,25,25,25,0,0,0,25,25,25,0,0,0,25,25,25,0,0,0,0,0,0,25,25,25,25,25,25,25,25,25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,25,0,25,0,25,0,0]
+        print("\033[7msmolPaint\033[0m started. Use transfer() to push new image.")
+        self.draw_bitmap(self.logo)
+        utime.sleep(1)
 
-        # Create a window
-        self.window = tk.Tk()
-        self.window.title("smolPaint")
-        self.window.geometry('320x260') # Set the window size to 800x600
-        self.window.resizable(False, False)
-        self.custom_font = font.Font(family="IBM 3270", size=10)
-        self.window.option_add("*Font", self.custom_font)
-        self.window.configure(bg="#222222")
-        self.window.iconphoto(True, tk.PhotoImage(file=icon_path))
+    def update_pixels(self,pixel_string):
+        pixel_values = pixel_string.split(',')
+        for i in range(25):
+            r = int(pixel_values[i*3])
+            g = int(pixel_values[i*3 + 1])
+            b = int(pixel_values[i*3 + 2])
+            self.pixels[24-i] = (r, g, b)
+        self.pixels.write()
 
-        # Create frames with padding
-        self.pixel_frame = tk.Frame(self.window, padx=10, pady=10)
-        self.pixel_frame.pack()
+    def transfer(self):
+        print("smolPaint: \033[7mSingle image mode\033[0m.\nInput pixels color stream")
+        self.draw_bitmap(self.logo)
+        while True:
+            data = input("25xR,G,B > ")
+            if len(data.split(','))==25*3:
+                self.update_pixels(data)
+            else:
+                print("Wrong data. 25*3 (r,g,b,) expected, got:",len(data.split(',')),data)
 
-        # Create Tab Control
-        self.tab_control = ttk.Notebook(self.window)
+    def animator(self):
+        print("smolPaint: \033[7mAnimator mode\033[0m.\nPaste frames. Hit enter (empty) to preview animation.")
+        self.draw_bitmap(self.logo)
+        frames=[]
+        current_frame=0
+        while True:
+            data = input("25xR,G,B > ")
+            if data=="":
+                for frame in frames:
+                    self.update_pixels(frame)
+                    utime.sleep(0.1)
+            if data=="clear":
+                frames=[]
+            else:
+                if len(data.split(','))==25*3:
+                    frames.append(data)
+                    for frame in frames:
+                        self.update_pixels(frame)
+                        utime.sleep(0.1)
+                else:
+                    print("Wrong data. 25*3 (r,g,b,) expected, got:",len(data.split(',')),data)
 
-        # Create tabs
-        self.tab1 = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab1, text='Primary Color')
+    def draw_bitmap(self,bitmap):
+        for i in range(25):
+            r = int(bitmap[i*3])
+            g = int(bitmap[i*3 + 1])
+            b = int(bitmap[i*3 + 2])
+            self.pixels[24-i] = (r, g, b)
+        self.pixels.write()
+        
+    def gallery(self):
+        bitmaps = []
+        # apple
+        bitmaps.append([0,0,0,0,0,0,0,0,0,3,0,0,3,0,0,0,0,0,0,0,0,27,16,1,27,16,1,20,0,0,0,0,0,11,24,1,0,16,1,33,0,0,33,0,0,11,24,1,0,0,0,33,0,0,33,0,0,20,0,0,0,0,0,0,0,0,0,0,0,3,0,0,3,0,0])
+        # cherry
+        bitmaps.append([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,22,0,25,25,0,16,0,0,0,0,0,0,9,19,1,0,12,1,25,0,11,25,0,16,9,19,1,0,0,0,20,0,16,25,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        # debian
+        bitmaps.append([0,0,0,25,0,0,25,0,0,25,0,0,0,0,0,25,0,0,0,0,0,0,0,0,0,0,0,25,0,0,25,0,0,0,0,0,6,0,0,6,0,0,0,0,0,25,0,0,0,0,0,2,0,0,25,0,0,0,0,0,0,0,0,25,0,0,25,0,0,0,0,0,0,0,0])
+        
+        print("smolPaint: \033[7mGallery mode\033[0m.")
+        for pic in bitmaps:
+            self.draw_bitmap(pic)
+            utime.sleep(1)
+            
+sp = smolPaint()
+sp.gallery()
+sp.transfer()
 
-        self.tab2 = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab2, text='Secondary Color')
-
-        self.tab3 = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab3, text='NeoPixel')
-
-        self.tab_tools = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab_tools, text='Clear')
-
-        self.tab_control.pack(expand=1, fill="both")
-
-        self.brightness_slider = Scale(self.tab3, from_=0, to=100, orient='horizontal', label='Brightness (%)')
-        self.brightness_slider.set(10)
-        self.brightness_slider.grid(row=0, column=0, padx=5, pady=4, sticky='ns')
-        self.brightness_slider.bind("<ButtonRelease-1>", self.update_pixel_stream)
-
-        self.pixel_stream_frame = ttk.Frame(self.tab3)
-        self.pixel_stream_frame.grid(row=0, column=1, padx=5, pady=4)
-
-        # Create a string representation of the pixel colors
-        self.pixel_stream = ["0,0,0"]*25  # Initialize all pixels as black
-        self.pixel_colors = tk.StringVar()
-        self.pixel_colors.set(",".join(self.pixel_stream))  # Display black for all pixels initially
-        self.pixel_colors_entry = tk.Entry(self.pixel_stream_frame, textvariable=self.pixel_colors, state='readonly', width=24)
-        self.pixel_colors_entry.pack(pady=4)
-
-        self.copy_button = tk.Button(self.pixel_stream_frame, text="Copy to Clipboard", command=self.copy_to_clipboard)
-        self.copy_button.pack(pady=4)
-
-        self.white_button = tk.Button(self.tab_tools, text="Clear to White", command=lambda: self.clear_grid("white"))
-        self.black_button = tk.Button(self.tab_tools, text="Clear to Black", command=lambda: self.clear_grid("black"))
-        self.white_button.config(bg="white",fg="black")
-        self.black_button.config(bg="black",fg="white")
-
-        self.white_button.pack(side='left', padx=5)
-        self.black_button.pack(side='left', padx=5)
-
-        # Define a 5x5 grid of buttons
-        self.buttons = []
-        for i in range(5):
-            row = []
-            for j in range(5):
-                button = tk.Button(self.pixel_frame, width=1, height=1, bg="black", bd=0)
-                button.grid(row=i, column=j, padx=0, pady=0)
-                row.append(button)
-            self.buttons.append(row)
-
-        self.controls = {}
-
-        for idx, tab in enumerate([self.tab1, self.tab2]):
-            slider_R = Scale(tab, from_=0, to=255, orient='horizontal', label='Red', width=20)
-            slider_G = Scale(tab, from_=0, to=255, orient='horizontal', label='Green', width=20)
-            slider_B = Scale(tab, from_=0, to=255, orient='horizontal', label='Blue', width=20)
-
-            slider_R.set((1-idx)*255)
-            slider_G.set((1-idx)*255)
-            slider_B.set((1-idx)*255)
-
-            slider_R.grid(row=0, column=0)
-            slider_G.grid(row=0, column=1)
-            slider_B.grid(row=0, column=2)
-
-            prev_color = "white"
-            if idx==1:
-                prev_color = "black"
-            color_preview = tk.Label(tab, text=" ", bg=prev_color, width=24)
-            color_preview.grid(row=1, column=0, columnspan=3)
-
-            self.controls[idx] = {
-                'slider_R': slider_R,
-                'slider_G': slider_G,
-                'slider_B': slider_B,
-                'color_preview': color_preview
-            }
-
-            for color in ['R', 'G', 'B']:
-                    self.controls[idx]['slider_'+color].bind("<ButtonRelease-1>",lambda event, idx=idx: self.update_color_preview(idx, event))
-
-        for i in range(5):
-            for j in range(5):
-                self.buttons[i][j].bind('<Button-1>', lambda event, i=i, j=j: self.change_color(i, j, 0))  # 0 - primary color
-                self.buttons[i][j].bind('<Button-3>', lambda event, i=i, j=j: self.change_color(i, j, 1))  # 1 - secondary color
-
-    def update_pixel_stream(self, event=None):
-        brightness = self.brightness_slider.get() / 100.0  # Convert percentage to ratio
-        adjusted_pixel_stream = []
-        for pixel in self.pixel_stream:
-            R, G, B = map(int, pixel.split(','))
-            R = int(R * brightness)
-            G = int(G * brightness)
-            B = int(B * brightness)
-            adjusted_pixel_stream.append(",".join([str(R), str(G), str(B)]))
-
-        self.pixel_colors.set(",".join(adjusted_pixel_stream))
-        self.pixel_colors_entry.update()
-
-    def copy_to_clipboard(self):
-        self.window.clipboard_clear()
-        self.window.clipboard_append(self.pixel_colors.get())
-
-    def clear_grid(self, color):
-        for i in range(5):
-            for j in range(5):
-                self.buttons[i][j].config(bg=color)
-
-        if color=="black":
-            self.pixel_stream = ["0,0,0"]*25
-        if color=="white":
-            self.pixel_stream = ["255,255,255"]*25
-
-        self.update_pixel_stream()
-
-    def update_color_preview(self, idx, _):
-        R = self.controls[idx]['slider_R'].get()
-        G = self.controls[idx]['slider_G'].get()
-        B = self.controls[idx]['slider_B'].get()
-        color = '#%02x%02x%02x' % (R, G, B)
-        self.controls[idx]['color_preview'].config(bg=color)
-
-
-    def change_color(self, i, j, idx):
-        R = self.controls[idx]['slider_R'].get()
-        G = self.controls[idx]['slider_G'].get()
-        B = self.controls[idx]['slider_B'].get()
-
-        color = '#%02x%02x%02x' % (R, G, B)
-        self.buttons[i][j].config(bg=color)
-
-        # Update the pixel color in the pixel stream
-        self.pixel_stream[j*5+i] = f"{R},{G},{B}"
-        self.update_pixel_stream()
-
-    def run(self):
-        self.window.mainloop()
-
-if __name__ == "__main__":
-    paint = smolPaint()
-    paint.run()
 
